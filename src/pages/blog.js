56cc77c84ad25157bdaa6720c79df73500e89032
IanGrubb <hi@iangrubb.com>
2020-07-16 18:41:27 -0400

    initial commit

---
+++ src/pages/blog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from "../components/seo"
+
+import styled from 'styled-components'
+
+const BlogPage = () => {
+    return (
+        <Layout>
+            <SEO title="Blog" />
+            <h1>Blog</h1>
+        </Layout>
+    )
+}
+
+
+export default BlogPage
---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from "../components/seo"
+
+const ProjectsPage = () => {
+    return (
+        <Layout>
+            <SEO title="Projects" />
+            <h1>Projects</h1>
+        </Layout>
+    )
+}
+
+export default ProjectsPage
